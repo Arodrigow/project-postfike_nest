055ca879642000a3ddfec6245e40807eb8e321ac
André Rodrigo B. A. Maciel <rodrigow_@hotmail.com.br>
2021-08-06 11:31:00 -0300

    Added new services to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PostModule } from './post/post.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PostModule, UserModule, AuthModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
