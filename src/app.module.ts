53eb64dff9af50102e94ba934da1ea6c16fe2815
André Rodrigo B. A. Maciel <rodrigow_@hotmail.com.br>
2021-08-09 09:56:19 -0300

    [Added] new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { PostModule } from './post/post.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { TagModule } from './tag/tag.module';
+import { ImagesModule } from './images/images.module';
 
 @Module({
-  imports: [PostModule, UserModule, AuthModule],
+  imports: [PostModule, UserModule, AuthModule, TagModule, ImagesModule],
   controllers: [],
   providers: [],
 })
